7f2f09dd1e4534f9a370bab5e54f6fd8acdea3c4
Mel Schultz <100803766+mel-schultz@users.noreply.github.com>
2026-04-16 20:11:19 -0300

    Update schema.ts

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,5 +1,5 @@
-//import { int, mysqlEnum, mysqlTable, text, timestamp, varchar, decimal, boolean } from "drizzle-orm/mysql-core";
-import { pgTable, serial, varchar, text, timestamp, integer, numeric, boolean, pgEnum } from "drizzle-orm/pg-core";
+import { int, mysqlEnum, mysqlTable, text, timestamp, varchar, decimal, boolean } from "drizzle-orm/mysql-core";
+
 
 /**
  * Core user table backing auth flow.
